ddb502eb167c6715a401c1559b9c41744d68505e
Ian Lim <ian@Ians-MacBook-Air.local>
2022-05-09 17:17:38 +0900

    minor modification

--- bin/amazon-chime-sma-bridging.ts
+++ bin/amazon-chime-sma-bridging.ts
@@ -1,9 +1,10 @@
 //  “Copyright Amazon.com Inc. or its affiliates.”
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-import { SmaBridgingDemo } from "../lib/amazon-chime-sma-bridging-stack";
+import { SmaSlackIntegDemo } from "../lib/amazon-chime-sma-slack-integ-stack";
 // import { ChimeToSlackDemo } from "../lib/chime-to-slack-stack";
 
 const app = new cdk.App();
-new SmaBridgingDemo(app, "SmaBridgingDemo", {});
+new SmaSlackIntegDemo(app, "SmaSlackIntegDemo", {});
 // new ChimeToSlackDemo(app, "ChimeToSlackDemo", {});
+
